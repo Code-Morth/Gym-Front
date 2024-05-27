111db1ff91c7c0fe473795074237dce13514d0dd
CodeMorth <kevinsalinasvd@gmail.com>
2024-05-27 17:35:57 -0500

    login

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,5 +1,9 @@
+"use client"
+import { useSearchParams } from "next/navigation"
 export default function Layout({ admin, user }: any) {
-  let routes = "admin"
+  const router = useSearchParams()
+
+  let routes = router.get("data")
 
   return <div>{routes === "admin" ? admin : user}</div>
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,39 @@
+"use client"
+import React, { useState } from "react"
+import { useRouter } from "next/navigation"
+
 export default function Home() {
+  const [formData, setFormData] = useState({ username: "", password: "" })
+
+  const router = useRouter()
+
+  const handleLogin = (event: any) => {
+    event.preventDefault()
+
+    const dataForm = Object.fromEntries(new FormData(event.target))
+    
+    dataForm.username === "admin" ? router.push("/dashboard?data=admin") : router.push("/dashboard?data=user")
+  }
+
   return (
-    <main>
-    Hola soy el main page
-    </main>
-  );
+    <>
+      <div className="loginContainer">
+        <div className="loginBox">
+          <h2>Iniciar Sesión</h2>
+          <form onSubmit={handleLogin}>
+            <div className="inputGroup">
+              <label htmlFor="username">Usuario</label>
+              <input type="text" name="username" required />
+            </div>
+            <div className="inputGroup">
+              <label htmlFor="password">Contraseña</label>
+              <input type="password" name="password" required />
+            </div>
+            {/* {error && <p className="error">{error}</p>} */}
+            <button className="button">Entrar</button>
+          </form>
+        </div>
+      </div>
+    </>
+  )
 }
